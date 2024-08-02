5419e3503ffa7910ead7586477d5e03b5451ae2b
Cloud <121166658+cloudedviper@users.noreply.github.com>
2024-08-02 02:22:58 -0500

    Welcome.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import Welcome from './Welcome';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Hello, React!</h1>
+        <Welcome name="Cloud" />
+        <Welcome name="Renee" />
       </header>
     </div>
   );
