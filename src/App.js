bd863b6c9f9180e0fef16c8aaa894700dcb9590e
Cloud <121166658+cloudedviper@users.noreply.github.com>
2024-08-04 03:55:25 -0500

    header.js

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import './App.css';
 import Welcome from './Welcome';
 import logo from './Assets/Images/logo.jpg';
 
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
         <h1>Hello, React!</h1>
         <Welcome name="Cloud" />
         <Welcome name="Renee" />
         <img src={logo} alt="Logo" />
-      </header>
     </div>
   );
 }
+
 
 export default App;
