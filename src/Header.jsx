6022d4a7a76b10b80021861b21310caf3ad6bcd3
Cloud <121166658+cloudedviper@users.noreply.github.com>
2024-08-10 04:41:07 -0500

    header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <header className="App-header"><h1>This is text</h1></header>
+    <header className="App-header"><h1>Text</h1></header>
   )
 }
